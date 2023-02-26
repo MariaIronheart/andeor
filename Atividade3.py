3486a320525297c48a9155642180d6eef7605f68
MariaIronheart <vic_a4@hotmail.com>
2023-02-26 13:09:35 -0300

    AndeOr git commit -m andeor git commit -m AndeOr

---
+++ Atividade3.py
@@ -0,0 +1,8 @@
+a = int(input("Insira um número: "))
+
+if a % 2 == 0:
+    print ("O número é par")
+elif a % 3 == 0:
+    print("O número é divisível por 3")
+else: 
+    print("O número não é divisível por 3 nem é par")
